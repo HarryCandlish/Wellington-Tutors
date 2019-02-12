4b4feecac0c0bd3cebf600ee50592a5c19223ad4
HarryCandlish <hcnd90@gmail.com>
2019-02-12 14:26:11 +1300

    changing stuff

--- client/components/Fiction.jsx
+++ client/components/Fiction.jsx
@@ -1,18 +1,46 @@
-import React from 'react'
+import React, {Component} from 'react'
+const request = require('superagent');
 
 import Home from './Home'
 
-const Fiction = () => {
+class Fiction extends Component {
+  constructor(){
+    super()
+    this.state = {
+      books: []
+    }
+  }
+  componentWillMount(){
+      request
+      .get("https://api.nytimes.com/svc/books/v3/lists.json?list-name=hardcover-fiction&api-key=FjSPdfzujduYkg5G6AvAbAAjYnozt8pc")
+      .end((err,res) => {
+        this.setState({books:res.body.results})
+      })
+    
+    
+  }
+  render() {
     return (
       <div>
         <div>
             <h1>Fiction</h1>
         </div>
         <div>
+          {
+            this.state.books.map((book) =>  {
+              return book.book_details[0].title
+
+            })
+          }
+        </div>
+        <div>
              <p><a href='./#'>Home</a></p>  
         </div>
       </div>
     )
+
+  }
 }
 
 export default Fiction
+
