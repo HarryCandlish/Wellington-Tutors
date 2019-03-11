781149b8a6e15b7b690f091de34c20cd0235a31f
HarryCandlish <hcnd90@gmail.com>
2019-03-12 10:51:17 +1300

    deleted unnecessary files for portfolio

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,29 +1,12 @@
-import React from 'react'
-import {HashRouter as Router, Route} from 'react-router-dom'
+import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
 
-import Home from './Home'
-import Nav from './Nav'
-import Fiction from './Fiction'
-import NonFiction from './NonFiction'
-import Profile from './Profile'
-import About from './About'
+const App = () => {
+  return (
+    <div>
+      <h1>Portfolio</h1>
+    </div>
+  );
+};
 
-  const App = () => {
-    return (
-      <Router>
-          <div>
-            <Route exact path='/' component={Home}/>
-            <Route exact path='/' component={Nav}/>
-            <Route path='/Fiction' component={Fiction}/>
-            <Route path='/NonFiction' component={NonFiction}/>
-            <Route path ='/Profile' component={Profile}/>
-            <Route path ='/About' component={About}/>
-          </div>
-      </Router>
-    )
-  } 
-
-
-
-export default App
-
+export default App;
