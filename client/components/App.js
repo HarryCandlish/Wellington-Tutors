e97e4e5d1b657ceefae35390fb650f5e7e917306
HarryCandlish <hcnd90@gmail.com>
2019-03-18 09:47:36 +1300

    fixed boilerplate

--- client/components/App.js
+++ client/components/App.js
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Portfolio</h1>
+        <h1>Book_Journal</h1>
       </div>
     );
   }
--- client/index.js
+++ client/index.js
@@ -4,16 +4,10 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunkMiddleware from "redux-thunk";
 
-import reducers from "./reducers";
 import App from "./components/App";
 
-import "./styles/main.scss";
-
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(
-  reducers,
-  composeEnhancers(applyMiddleware(thunkMiddleware))
-);
+const store = createStore(composeEnhancers(applyMiddleware(thunkMiddleware)));
 
 document.addEventListener("DOMContentLoaded", () => {
   render(
