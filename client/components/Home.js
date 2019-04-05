0bcd15d6491f601abccf28f86903329fb12fcf68
HarryCandlish <hcnd90@gmail.com>
2019-04-05 16:55:04 +1300

    completed framework for home-page

--- client/components/Home.js
+++ client/components/Home.js
@@ -9,6 +9,28 @@ class Home extends Component {
             <h1 className="heading">Central Tutoring</h1>
           </div>
         </div>
+        <div className="bodygrid">
+          <div className="topic1">
+            <h4>Profile</h4>
+          </div>
+          <div className="topic1">
+            <h4>Criteria</h4>
+          </div>
+          <div className="topic1">
+            <h4>Tutors</h4>
+          </div>
+          <div className="topic1">
+            <h4>Software</h4>
+          </div>
+        </div>
+        <div className="footergrid">
+          <div className="aboutgrid">
+            <h3>Our Purpose</h3>
+          </div>
+          <div className="aboutgrid">
+            <p>Help people learn how to code</p>
+          </div>
+        </div>
       </div>
     );
   }
