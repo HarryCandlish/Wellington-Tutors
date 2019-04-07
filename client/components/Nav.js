96b0cff756c6a20a80e650c05f07ee5d579195de
HarryCandlish <hcnd90@gmail.com>
2019-04-08 09:06:29 +1200

    created dropdown nav

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -5,26 +5,25 @@ class Nav extends Component {
     return (
       <div>
         <nav className="nav">
-          <div className="one">
-            <a href="#" className="navLinks">
-              Home
-            </a>
-          </div>
-          <div className="two">
-            <a href="#" className="navLinks">
-              Tutors
-            </a>
-          </div>
-          <div className="three">
-            <a href="#" className="navLinks">
-              About
-            </a>
-          </div>
-          <div className="four">
-            <a href="#" className="navLinks">
-              Guidelines
-            </a>
-          </div>
+          <ul>
+            <li>
+              <a href="#">Content</a>
+              <ul>
+                <li>
+                  <a href="#">Tutors</a>
+                </li>
+                <li>
+                  <a href="#">Software</a>
+                </li>
+                <li>
+                  <a href="#">Guidelines</a>
+                </li>
+                <li>
+                  <a href="#">About</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
         </nav>
       </div>
     );
