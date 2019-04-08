1fa2a3ff490d8c99194400eaea4a60c32d7382ee
HarryCandlish <hcnd90@gmail.com>
2019-04-09 11:22:53 +1200

    created grids for teachers-page

--- client/components/Teachers.js
+++ client/components/Teachers.js
@@ -4,7 +4,113 @@ class Teachers extends Component {
   render() {
     return (
       <div>
-        <h1>Teachers</h1>
+        <div className="topTeacherGrid">
+          <div className="teacherTitle">
+            <h1>Teachers</h1>
+          </div>
+        </div>
+        <div className="bodygrid">
+          <div className="topic1">
+            <a href="#">
+              <img className="images" src="images/profile.jpg" />
+            </a>
+            <div className="text-block">
+              <p className="titleGrids">Profile</p>
+            </div>
+          </div>
+          <div className="topic1">
+            <a href="#">
+              <img className="images" src="images/curriculum.jpg" />
+            </a>
+            <div className="text-block">
+              <p className="titleGrids">Curriculum</p>
+            </div>
+          </div>
+          <div className="topic1">
+            <a href="#">
+              <img className="images" src="images/tutors.jpg" />
+            </a>
+            <div className="text-block">
+              <p className="titleGrids">Tutors</p>
+            </div>
+          </div>
+          <div className="topic1">
+            <a href="#">
+              <img className="images" src="images/software.jpg" />
+            </a>
+            <div className="text-block">
+              <p className="titleGrids">Software</p>
+            </div>
+          </div>
+        </div>
+        <div className="bodygrid">
+          <div className="topic1">
+            <a href="#">
+              <img className="images" src="images/profile.jpg" />
+            </a>
+            <div className="text-block">
+              <p className="titleGrids">Profile</p>
+            </div>
+          </div>
+          <div className="topic1">
+            <a href="#">
+              <img className="images" src="images/curriculum.jpg" />
+            </a>
+            <div className="text-block">
+              <p className="titleGrids">Curriculum</p>
+            </div>
+          </div>
+          <div className="topic1">
+            <a href="#">
+              <img className="images" src="images/tutors.jpg" />
+            </a>
+            <div className="text-block">
+              <p className="titleGrids">Tutors</p>
+            </div>
+          </div>
+          <div className="topic1">
+            <a href="#">
+              <img className="images" src="images/software.jpg" />
+            </a>
+            <div className="text-block">
+              <p className="titleGrids">Software</p>
+            </div>
+          </div>
+        </div>
+        <div className="bodygrid">
+          <div className="topic1">
+            <a href="#">
+              <img className="images" src="images/profile.jpg" />
+            </a>
+            <div className="text-block">
+              <p className="titleGrids">Profile</p>
+            </div>
+          </div>
+          <div className="topic1">
+            <a href="#">
+              <img className="images" src="images/curriculum.jpg" />
+            </a>
+            <div className="text-block">
+              <p className="titleGrids">Curriculum</p>
+            </div>
+          </div>
+          <div className="topic1">
+            <a href="#">
+              <img className="images" src="images/tutors.jpg" />
+            </a>
+            <div className="text-block">
+              <p className="titleGrids">Tutors</p>
+            </div>
+          </div>
+          <div className="topic1">
+            <a href="#">
+              <img className="images" src="images/software.jpg" />
+            </a>
+            <div className="text-block">
+              <p className="titleGrids">Software</p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
