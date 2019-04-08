6bdb5ed69c15ebce339d7df40d88e5b3ea87628e
HarryCandlish <hcnd90@gmail.com>
2019-04-09 11:34:08 +1200

    added text box to teachers profiles

--- client/components/Teachers.js
+++ client/components/Teachers.js
@@ -9,105 +9,153 @@ class Teachers extends Component {
             <h1>Teachers</h1>
           </div>
         </div>
-        <div className="bodygrid">
-          <div className="topic1">
+        <div className="teachersbodygrid">
+          <div className="topic2">
             <a href="#">
               <img className="images" src="images/profile.jpg" />
             </a>
             <div className="text-block">
-              <p className="titleGrids">Profile</p>
+              <p className="titleGrids">Henry Miller</p>
+              <br />
+              <p>About: Like sitting under the palm trees in California.</p>
+              <br />
+              <p>Skills: React and Redux</p>
             </div>
           </div>
-          <div className="topic1">
+          <div className="topic2">
             <a href="#">
               <img className="images" src="images/curriculum.jpg" />
             </a>
             <div className="text-block">
-              <p className="titleGrids">Curriculum</p>
+              <p className="titleGrids">Sylvia Plath</p>
+              <br />
+              <p>About: Like sitting under the palm trees in California.</p>
+              <br />
+              <p>Skills: React and Redux</p>
             </div>
           </div>
-          <div className="topic1">
+          <div className="topic2">
             <a href="#">
               <img className="images" src="images/tutors.jpg" />
             </a>
             <div className="text-block">
-              <p className="titleGrids">Tutors</p>
+              <p className="titleGrids">Fyodor Dostoevsky</p>
+              <br />
+              <p>About: Like sitting under the palm trees in California.</p>
+              <br />
+              <p>Skills: React and Redux</p>
             </div>
           </div>
-          <div className="topic1">
+          <div className="topic2">
             <a href="#">
               <img className="images" src="images/software.jpg" />
             </a>
             <div className="text-block">
-              <p className="titleGrids">Software</p>
+              <p className="titleGrids">Milan Kundera</p>
+              <br />
+              <p>About: Like sitting under the palm trees in California.</p>
+              <br />
+              <p>Skills: React and Redux</p>
             </div>
           </div>
         </div>
-        <div className="bodygrid">
-          <div className="topic1">
+        <div className="teachersbodygrid">
+          <div className="topic2">
             <a href="#">
               <img className="images" src="images/profile.jpg" />
             </a>
             <div className="text-block">
-              <p className="titleGrids">Profile</p>
+              <p className="titleGrids">Charles Bukowski</p>
+              <br />
+              <p>About: Like sitting under the palm trees in California.</p>
+              <br />
+              <p>Skills: React and Redux</p>
             </div>
           </div>
-          <div className="topic1">
+          <div className="topic2">
             <a href="#">
               <img className="images" src="images/curriculum.jpg" />
             </a>
             <div className="text-block">
-              <p className="titleGrids">Curriculum</p>
+              <p className="titleGrids">Albert Camus</p>
+              <br />
+              <p>About: Like sitting under the palm trees in California.</p>
+              <br />
+              <p>Skills: React and Redux</p>
             </div>
           </div>
-          <div className="topic1">
+          <div className="topic2">
             <a href="#">
               <img className="images" src="images/tutors.jpg" />
             </a>
             <div className="text-block">
-              <p className="titleGrids">Tutors</p>
+              <p className="titleGrids">Friedrich Nietzsche</p>
+              <br />
+              <p>About: Like sitting under the palm trees in California.</p>
+              <br />
+              <p>Skills: React and Redux</p>
             </div>
           </div>
-          <div className="topic1">
+          <div className="topic2">
             <a href="#">
               <img className="images" src="images/software.jpg" />
             </a>
             <div className="text-block">
-              <p className="titleGrids">Software</p>
+              <p className="titleGrids">Graham Greene</p>
+              <br />
+              <p>About: Like sitting under the palm trees in California.</p>
+              <br />
+              <p>Skills: React and Redux</p>
             </div>
           </div>
         </div>
-        <div className="bodygrid">
-          <div className="topic1">
+        <div className="teachersbodygrid">
+          <div className="topic2">
             <a href="#">
               <img className="images" src="images/profile.jpg" />
             </a>
             <div className="text-block">
-              <p className="titleGrids">Profile</p>
+              <p className="titleGrids">Frans Kafka</p>
+              <br />
+              <p>About: Like sitting under the palm trees in California.</p>
+              <br />
+              <p>Skills: React and Redux</p>
             </div>
           </div>
-          <div className="topic1">
+          <div className="topic2">
             <a href="#">
               <img className="images" src="images/curriculum.jpg" />
             </a>
             <div className="text-block">
-              <p className="titleGrids">Curriculum</p>
+              <p className="titleGrids">Hermann Hesse</p>
+              <br />
+              <p>About: Like sitting under the palm trees in California.</p>
+              <br />
+              <p>Skills: React and Redux</p>
             </div>
           </div>
-          <div className="topic1">
+          <div className="topic2">
             <a href="#">
               <img className="images" src="images/tutors.jpg" />
             </a>
             <div className="text-block">
-              <p className="titleGrids">Tutors</p>
+              <p className="titleGrids">Virginia Wolf</p>
+              <br />
+              <p>About: Like sitting under the palm trees in California.</p>
+              <br />
+              <p>Skills: React and Redux</p>
             </div>
           </div>
-          <div className="topic1">
+          <div className="topic2">
             <a href="#">
               <img className="images" src="images/software.jpg" />
             </a>
             <div className="text-block">
-              <p className="titleGrids">Software</p>
+              <p className="titleGrids">John Fante</p>
+              <br />
+              <p>About: Like sitting under the palm trees in California.</p>
+              <br />
+              <p>Skills: React and Redux</p>
             </div>
           </div>
         </div>
