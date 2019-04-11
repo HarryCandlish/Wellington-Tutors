3d1bdd68e25f02e9df2b9bade742ae30efedded8
HarryCandlish <hcnd90@gmail.com>
2019-04-11 19:04:55 +1200

    student profile framework completed

--- client/components/Dashboard.js
+++ client/components/Dashboard.js
@@ -7,9 +7,26 @@ class Dashboard extends Component {
         <div className="profileTitleGrid">
           <div className="profileHeaderGrid">
             <div>
-              <h2>Profile</h2>
+              <h2>Student Profile</h2>
             </div>
             <img className="profileTitleImage" src="images/profilepage.jpg" />
+            <div className="profileBodyGrid">
+              <div className="profileContentOne">
+                <h3>Tutors</h3>
+                <p>Associated</p>
+              </div>
+              <div className="profileContentTwo">
+                <h3>About</h3>
+                <p>
+                  Attempting to gain a better understanding of software
+                  development
+                </p>
+              </div>
+              <div className="profileContentThree">
+                <h3>Technologies</h3>
+                <p>Learnt</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
