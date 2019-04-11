af7826c1e9a99d3ab37763d1d7ef1144d64946a6
HarryCandlish <hcnd90@gmail.com>
2019-04-11 16:23:02 +1200

    created tech page

--- client/components/Technologies.js
+++ client/components/Technologies.js
@@ -4,9 +4,125 @@ class Technologies extends Component {
   render() {
     return (
       <div>
-        <div className="techGrid">
-          <div className="techHeader">
-            <h2 className="techTitle">Technologies</h2>
+        <div>
+          <div className="techGrid">
+            <div className="techHeader">
+              <h2 className="techTitle">Technologies</h2>
+            </div>
+          </div>
+        </div>
+        <div className="techbodygrid">
+          <div className="topic3">
+            <a href="#">
+              <img className="images" src="images/profile.jpg" />
+            </a>
+            <div className="text-block3">
+              <h3 className="titleGrids">React</h3>
+              <p>Description:</p>
+            </div>
+          </div>
+          <div className="topic3">
+            <a href="#">
+              <img className="images" src="images/curriculum.jpg" />
+            </a>
+            <div className="text-block3">
+              <h3 className="titleGrids">Redux</h3>
+              <p>Description:</p>
+            </div>
+          </div>
+          <div className="topic3">
+            <a href="#">
+              <img className="images" src="images/tutors.jpg" />
+            </a>
+            <div className="text-block3">
+              <h3 className="titleGrids">CSS/HTML</h3>
+              <p>Description:</p>
+            </div>
+          </div>
+          <div className="topic3">
+            <a href="#">
+              <img className="images" src="images/software.jpg" />
+            </a>
+            <div className="text-block3">
+              <h3 className="titleGrids">Databases</h3>
+              <p>Description:</p>
+            </div>
+          </div>
+        </div>
+        <div className="techbodygrid">
+          <div className="topic3">
+            <a href="#">
+              <img className="images" src="images/profile.jpg" />
+            </a>
+            <div className="text-block3">
+              <h3 className="titleGrids">Back-End</h3>
+              <p>Description:</p>
+            </div>
+          </div>
+          <div className="topic3">
+            <a href="#">
+              <img className="images" src="images/curriculum.jpg" />
+            </a>
+            <div className="text-block3">
+              <h3 className="titleGrids">Front-End</h3>
+              <p>Description:</p>
+            </div>
+          </div>
+          <div className="topic3">
+            <a href="#">
+              <img className="images" src="images/tutors.jpg" />
+            </a>
+            <div className="text-block3">
+              <h3 className="titleGrids">Full-Stack</h3>
+              <p>Description:</p>
+            </div>
+          </div>
+          <div className="topic3">
+            <a href="#">
+              <img className="images" src="images/software.jpg" />
+            </a>
+            <div className="text-block3">
+              <h3 className="titleGrids">Web-Design</h3>
+              <p>Description:</p>
+            </div>
+          </div>
+        </div>
+        <div className="techbodygrid">
+          <div className="topic3">
+            <a href="#">
+              <img className="images" src="images/profile.jpg" />
+            </a>
+            <div className="text-block3">
+              <h3 className="titleGrids">Testing</h3>
+              <p>Description:</p>
+            </div>
+          </div>
+          <div className="topic3">
+            <a href="#">
+              <img className="images" src="images/curriculum.jpg" />
+            </a>
+            <div className="text-block3">
+              <h3 className="titleGrids">PHP</h3>
+              <p>Description:</p>
+            </div>
+          </div>
+          <div className="topic3">
+            <a href="#">
+              <img className="images" src="images/tutors.jpg" />
+            </a>
+            <div className="text-block3">
+              <h3 className="titleGrids">Ruby</h3>
+              <p>Description:</p>
+            </div>
+          </div>
+          <div className="topic3">
+            <a href="#">
+              <img className="images" src="images/software.jpg" />
+            </a>
+            <div className="text-block3">
+              <h3 className="titleGrids">React Native</h3>
+              <p>Description:</p>
+            </div>
           </div>
         </div>
       </div>
--- client/components/Tutors.js
+++ client/components/Tutors.js
@@ -4,22 +4,22 @@ class Tutors extends Component {
   render() {
     return (
       <div>
-        <div className="topTeacherGrid">
-          <div className="teacherTitle">
-            <h1>Tutors</h1>
+        <div>
+          <div className="tutorGrid">
+            <div className="tutorHeader">
+              <h2 className="tutorTitle">Tutors</h2>
+            </div>
           </div>
         </div>
-        <div className="teachersbodygrid">
+        <div className="tutorbodygrid">
           <div className="topic2">
             <a href="#">
               <img className="images" src="images/profile.jpg" />
             </a>
             <div className="text-block2">
-              <p className="titleGrids">Henry Miller</p>
-              <br />
-              <p>About: Like sitting under the palm trees in California.</p>
-              <br />
-              <p>Skills: React and Redux</p>
+              <h3 className="titleGrids">Name:</h3>
+              <p>About:</p>
+              <p>Skills:</p>
             </div>
           </div>
           <div className="topic2">
@@ -27,11 +27,9 @@ class Tutors extends Component {
               <img className="images" src="images/curriculum.jpg" />
             </a>
             <div className="text-block2">
-              <p className="titleGrids">Sylvia Plath</p>
-              <br />
-              <p>About: Like sitting under the palm trees in California.</p>
-              <br />
-              <p>Skills: React and Redux</p>
+              <h3 className="titleGrids">Name:</h3>
+              <p>About:</p>
+              <p>Skills:</p>
             </div>
           </div>
           <div className="topic2">
@@ -39,11 +37,9 @@ class Tutors extends Component {
               <img className="images" src="images/tutors.jpg" />
             </a>
             <div className="text-block2">
-              <p className="titleGrids">Fyodor Dostoevsky</p>
-              <br />
-              <p>About: Like sitting under the palm trees in California.</p>
-              <br />
-              <p>Skills: React and Redux</p>
+              <h3 className="titleGrids">Name:</h3>
+              <p>About:</p>
+              <p>Skills:</p>
             </div>
           </div>
           <div className="topic2">
@@ -51,25 +47,21 @@ class Tutors extends Component {
               <img className="images" src="images/software.jpg" />
             </a>
             <div className="text-block2">
-              <p className="titleGrids">Milan Kundera</p>
-              <br />
-              <p>About: Like sitting under the palm trees in California.</p>
-              <br />
-              <p>Skills: React and Redux</p>
+              <h3 className="titleGrids">Name:</h3>
+              <p>About:</p>
+              <p>Skills:</p>
             </div>
           </div>
         </div>
-        <div className="teachersbodygrid">
+        <div className="tutorbodygrid">
           <div className="topic2">
             <a href="#">
               <img className="images" src="images/profile.jpg" />
             </a>
             <div className="text-block2">
-              <p className="titleGrids">Charles Bukowski</p>
-              <br />
-              <p>About: Like sitting under the palm trees in California.</p>
-              <br />
-              <p>Skills: React and Redux</p>
+              <h3 className="titleGrids">Name:</h3>
+              <p>About:</p>
+              <p>Skills:</p>
             </div>
           </div>
           <div className="topic2">
@@ -77,11 +69,9 @@ class Tutors extends Component {
               <img className="images" src="images/curriculum.jpg" />
             </a>
             <div className="text-block2">
-              <p className="titleGrids">Albert Camus</p>
-              <br />
-              <p>About: Like sitting under the palm trees in California.</p>
-              <br />
-              <p>Skills: React and Redux</p>
+              <h3 className="titleGrids">Name:</h3>
+              <p>About:</p>
+              <p>Skills:</p>
             </div>
           </div>
           <div className="topic2">
@@ -89,11 +79,9 @@ class Tutors extends Component {
               <img className="images" src="images/tutors.jpg" />
             </a>
             <div className="text-block2">
-              <p className="titleGrids">Friedrich Nietzsche</p>
-              <br />
-              <p>About: Like sitting under the palm trees in California.</p>
-              <br />
-              <p>Skills: React and Redux</p>
+              <h3 className="titleGrids">Name:</h3>
+              <p>About:</p>
+              <p>Skills:</p>
             </div>
           </div>
           <div className="topic2">
@@ -101,25 +89,21 @@ class Tutors extends Component {
               <img className="images" src="images/software.jpg" />
             </a>
             <div className="text-block2">
-              <p className="titleGrids">Graham Greene</p>
-              <br />
-              <p>About: Like sitting under the palm trees in California.</p>
-              <br />
-              <p>Skills: React and Redux</p>
+              <h3 className="titleGrids">Name:</h3>
+              <p>About:</p>
+              <p>Skills:</p>
             </div>
           </div>
         </div>
-        <div className="teachersbodygrid">
-          <div className="topic2">
+        <div className="tutorbodygrid">
+          <div className="topic3">
             <a href="#">
               <img className="images" src="images/profile.jpg" />
             </a>
             <div className="text-block2">
-              <p className="titleGrids">Frans Kafka</p>
-              <br />
-              <p>About: Like sitting under the palm trees in California.</p>
-              <br />
-              <p>Skills: React and Redux</p>
+              <h3 className="titleGrids">Name:</h3>
+              <p>About:</p>
+              <p>Skills:</p>
             </div>
           </div>
           <div className="topic2">
@@ -127,11 +111,9 @@ class Tutors extends Component {
               <img className="images" src="images/curriculum.jpg" />
             </a>
             <div className="text-block2">
-              <p className="titleGrids">Hermann Hesse</p>
-              <br />
-              <p>About: Like sitting under the palm trees in California.</p>
-              <br />
-              <p>Skills: React and Redux</p>
+              <h3 className="titleGrids">Name:</h3>
+              <p>About:</p>
+              <p>Skills:</p>
             </div>
           </div>
           <div className="topic2">
@@ -139,11 +121,9 @@ class Tutors extends Component {
               <img className="images" src="images/tutors.jpg" />
             </a>
             <div className="text-block2">
-              <p className="titleGrids">Virginia Wolf</p>
-              <br />
-              <p>About: Like sitting under the palm trees in California.</p>
-              <br />
-              <p>Skills: React and Redux</p>
+              <h3 className="titleGrids">Name:</h3>
+              <p>About:</p>
+              <p>Skills:</p>
             </div>
           </div>
           <div className="topic2">
@@ -151,11 +131,9 @@ class Tutors extends Component {
               <img className="images" src="images/software.jpg" />
             </a>
             <div className="text-block2">
-              <p className="titleGrids">John Fante</p>
-              <br />
-              <p>About: Like sitting under the palm trees in California.</p>
-              <br />
-              <p>Skills: React and Redux</p>
+              <h3 className="titleGrids">Name:</h3>
+              <p>About:</p>
+              <p>Skills:</p>
             </div>
           </div>
         </div>
